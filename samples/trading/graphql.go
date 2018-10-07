5e8d87f9be5ef4500b67723b1908265454fb49e4
zhs007 <sssxueren@gmail.com>
2018-10-07 17:33:30 +0800

    upd

--- samples/trading/graphql.go
+++ samples/trading/graphql.go
@@ -5,6 +5,47 @@ import (
 
 	"github.com/graphql-go/graphql"
 	"github.com/zhs007/ankadb/graphqlext"
+
+	pb "github.com/zhs007/ankadb/samples/trading/proto"
+)
+
+var c = pb.Candle{
+	Open:         1,
+	Close:        2,
+	High:         3,
+	Low:          4,
+	Volume:       5,
+	OpenInterest: 6,
+	Curtime:      7,
+}
+
+var candleInputType = graphql.NewInputObject(
+	graphql.InputObjectConfig{
+		Name: "CandleInput",
+		Fields: graphql.InputObjectConfigFieldMap{
+			"curtime": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Timestamp,
+			},
+			"open": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Int64,
+			},
+			"close": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Int64,
+			},
+			"high": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Int64,
+			},
+			"low": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Int64,
+			},
+			"volume": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Int64,
+			},
+			"openInterest": &graphql.InputObjectFieldConfig{
+				Type: graphqlext.Int64,
+			},
+		},
+	},
 )
 
 // candleType - Candle
@@ -66,7 +107,7 @@ var queryType = graphql.NewObject(
 		Name: "Query",
 		Fields: graphql.Fields{
 			"candleChunks": &graphql.Field{
-				Type: graphql.NewList(candleChunkType),
+				Type: candleType,
 				Args: graphql.FieldConfigArgument{
 					"code": &graphql.ArgumentConfig{
 						Type: graphql.String,
@@ -86,23 +127,66 @@ var queryType = graphql.NewObject(
 					// if isOK {
 					// return data[idQuery], nil
 					// }
-					return nil, nil
+					return c, nil
 				},
 			},
 		},
 	},
 )
 
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"insertCandles": &graphql.Field{
+			Type:        candleChunkType,
+			Description: "insert candles",
+			Args: graphql.FieldConfigArgument{
+				"code": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"endTime": &graphql.ArgumentConfig{
+					Type: graphqlext.Timestamp,
+				},
+				"candle": &graphql.ArgumentConfig{
+					Type: candleInputType,
+				},
+				// "candles": &graphql.ArgumentConfig{
+				// 	Type: graphql.NewList(candleType),
+				// },
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				// rand.Seed(time.Now().UnixNano())
+				// product := Product{
+				// 	ID:    int64(rand.Intn(100000)), // generate random ID
+				// 	Name:  params.Args["name"].(string),
+				// 	Info:  params.Args["info"].(string),
+				// 	Price: params.Args["price"].(float64),
+				// }
+				// products = append(products, product)
+				return nil, nil
+			},
+		},
+	},
+})
+
+var curTypes = []graphql.Type{candleType}
+
 var schema, _ = graphql.NewSchema(
 	graphql.SchemaConfig{
-		Query: queryType,
+		Query:    queryType,
+		Mutation: mutationType,
+		Types:    curTypes,
 	},
 )
 
-func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+func executeQuery(query string, mapvar map[string]interface{}, schema graphql.Schema) *graphql.Result {
 	result := graphql.Do(graphql.Params{
-		Schema:        schema,
-		RequestString: query,
+		Schema:         schema,
+		RequestString:  query,
+		VariableValues: mapvar,
 	})
 	if len(result.Errors) > 0 {
 		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
--- samples/trading/main.go
+++ samples/trading/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
 
+	pb "github.com/zhs007/ankadb/samples/trading/proto"
+)
+
+func main() {
+	mapvar := make(map[string]interface{})
+
+	c := pb.Candle{
+		Open:         1,
+		Close:        2,
+		High:         3,
+		Low:          4,
+		Volume:       5,
+		OpenInterest: 6,
+		Curtime:      7,
+	}
+
+	c1 := make(map[string]interface{})
+	c1["open"] = 1
+	c1["close"] = 2
+	c1["high"] = 3
+	c1["low"] = 4
+	c1["volume"] = 5
+	c1["openInterest"] = 6
+	c1["curtime"] = 7
+
+	lstc := make([]pb.Candle, 1)
+	lstc[0] = c
+
+	mapvar["code"] = "pta"
+	mapvar["name"] = "pta1801"
+	mapvar["candles"] = lstc
+	mapvar["endTime"] = 0
+	mapvar["startTime"] = 0
+	mapvar["candle"] = c1
+
+	r := executeQuery(`
+	mutation InsertCandles($code:String!, $name:String!, $candle:CandleInput){
+		insertCandles(code:$code,name:$name,candle:$candle){
+			code,
+			name}
+		}`, mapvar, schema)
+	executeQuery("query GetAllData($code:String!, $name:String!, $startTime:Timestamp, $endTime:Timestamp){candleChunks(code:$code,name:$name,startTime:$startTime,endTime:$endTime){open,close}}", mapvar, schema)
+	b, _ := json.Marshal(r)
+	fmt.Println(string(b))
 }
