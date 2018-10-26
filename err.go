bf9028a31f761eb5890822affcfc4328ef8681cc
zhs007 <sssxueren@gmail.com>
2018-10-26 18:22:18 +0800

    upd

--- err.go
+++ err.go
@@ -11,4 +11,14 @@ var (
 	ErrLoadFileReadSize = errors.New("loadfile invalid file read size")
 	// ErrHTTPNoQuery - HTTP no query
 	ErrHTTPNoQuery = errors.New("HTTP no query")
+	// ErrCtxAnkaDB - context has not ankadb
+	ErrCtxAnkaDB = errors.New("context has not ankadb")
+	// ErrCtxCurDB - context has not currentdb
+	ErrCtxCurDB = errors.New("context has not currentdb")
+	// ErrQuertParams - query params err
+	ErrQuertParams = errors.New("query params err")
+	// ErrQuertResultDecode - query result decode err
+	ErrQuertResultDecode = errors.New("query result decode err")
+	// ErrCtxSnapshotMgr - query get snapshotMgr from err
+	ErrCtxSnapshotMgr = errors.New("query get snapshotMgr from err")
 )
