9482f2131abbd7e18c7b35cc86f0c8cc3f8d118a
zhs007 <sssxueren@gmail.com>
2019-02-28 17:50:00 +0800

    - updated unittest

--- starttesting.sh
+++
@@ -1,9 +0,0 @@
-rm -rf test/database_test
-rm -rf test/database_testasync
-rm -rf test/test001
-rm -rf test/test001-001
-rm -rf test/test001-002
-rm -rf test/test_msg
-rm -rf test/test_user
-
-go test ./... -v
---
+++ startunittest.sh
@@ -0,0 +1,1 @@
+docker build -f ./Dockerfile.unittest -t ankadb.unittest .
